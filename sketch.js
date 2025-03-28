8d778501747278cf6b3b60a7401ba26d8e0d3739
ssamfloyd <faircaity@gmail.com>
2025-03-28 14:34:16 -0600

    Week 9 done

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    circle(200,150,175)
+    square(130,125,50)
+    square(220,125,50)
+    line(180,150,220,150)
+    point(200,170)
+    triangle(180, 185, 188, 190, 196, 185)
+    square(112,240,180)
+    text('Sam Floyd',10,390)
+    text('Self Portrait',165,20)
+  }
